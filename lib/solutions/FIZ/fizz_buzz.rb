096f2131acc71c9a33e99ed99a0af6bc6cc55660
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:41:11 +0000

    Mon Apr 08 10:41:11 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,34 +3,29 @@ class FizzBuzz
 
   def fizz_buzz(number)
     num_as_string = number.to_s
-    result = { "fizz" => false , "buzz" => false, "deluxe" => false}
+    bools = { "fizz" => false , "buzz" => false, "deluxe" => false}
+    result = []
 
     if number % 5 == 0 || num_as_string.include?("5")
-      result["buzz"] = true
+      bools["buzz"] = true
     end
 
     if number % 3 == 0 || num_as_string.include?("3")
-      result["fizz"] = true
+      bools["fizz"] = true
     end
 
+    if num_as_string.count(num_as_string[1]) == num_as_string.length
+      bools["deluxe"] = true
+    end
 
-      if num_as_string.count(num_as_string[1]) == num_as_string.length
-
-
-
-    if result["fizz"] && result["buzz"]
-      "fizz buzz"
-    elsif result["fizz"]
-      "fizz"
-    elsif result["buzz"]
-      "buzz"
-    else
-      num_as_string
+    bools.each_pair do |key, value|
+      result << key if value
     end
 
   end
 
 end
+
 
 
 
