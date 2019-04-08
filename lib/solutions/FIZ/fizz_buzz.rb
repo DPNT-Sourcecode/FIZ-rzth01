ebcc9b4c0c860fd08cea6d2606f2068a24a31a52
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:03:47 +0000

    Mon Apr 08 10:03:47 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,7 +2,16 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    raise 'Not implemented'
+    if number % 15 == 0
+      "fizz buzz"
+    elsif number % 5 == 0
+      "buzz"
+    elsif number % 3 == 0
+      "fizz"
+    else
+      number
+    end
   end
 
 end
+
