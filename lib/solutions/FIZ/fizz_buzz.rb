678a91c5cab74433b2f767b8520969965a125ba9
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:25:28 +0000

    Mon Apr 08 10:25:28 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,12 +4,26 @@ class FizzBuzz
   def fizz_buzz(number)
     num_as_string = number.to_s
     result = { "fizz" => false , "buzz" => false}
-    if number % 15 == 0
+    if number % 5 == 0 || num_as_string.include?("5")
+      result["fizz"] = true
+    elsif number % 3 == 0 || num_as_string.include?("3")
+      result["buzz"] = true
+    end
 
+    if result["fizz"] && result["buzz"]
+      "fizz buzz"
+    elsif result["fizz"]
+      "fizz"
+    elsif result["bizz"]
+      "buzz"
+    else
+      num_as_string
+    end
 
   end
 
 end
+
 
 
 
